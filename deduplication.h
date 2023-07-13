64e81d714fb64234b323d39eabf21a4995cd3816
Thoin <814632021@qq.com>
2023-07-13 12:46:38 +0800

    remove the same data

---
+++ deduplication.h
@@ -0,0 +1,76 @@
+#pragma once
+/**************************************************
+* @This function is used to delete same data.
+* @Input     : several datas path
+* @Output    : one dataset with data deduplication
+* @Up-to-date: 2023-07-12 17:14
+**************************************************/
+
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+
+using std::vector;
+using std::string;
+using std::unordered_set;
+
+/** 数据去重 */
+unordered_set<string> Deduplication(const char* path) 
+{
+	string result;  // 数据格式—>字符串
+	unordered_set<string> data_lib;  // 存放字符串
+	unsigned int pos = 0;   // 当前数据索引
+	unsigned int diff = 0;  // 异同的数据量
+	unsigned int same = 0;  // 相同的数据量
+
+	std::ifstream ifs(path);
+
+	if (!ifs.is_open())
+	{
+		std::cout << "Can't open this file." << std::endl;
+		exit(-1);
+	}
+
+	while (!ifs.eof())
+	{
+		pos++;  // 数据索引+1
+		std::getline(ifs, result, '\n');
+		std::replace(result.begin(), result.end(), '\t', '/');  // '\t' -> '/'
+
+		// 相同
+		if (data_lib.count(result) != 0)
+		{
+			++same;
+			continue;
+		}
+		// 异同
+		data_lib.emplace(result);
+		++diff;
+	}
+	
+	std::cout << "Total：" << pos << "\n";
+	std::cout << "Differener：" << diff << "\n";
+	std::cout << "Same：" << same << std::endl;
+
+	ifs.close();
+	return data_lib;
+}
+
+/** 去除尾部空格 */
+void ExportFinalData(unordered_set<string>& lib, const char* path)
+{
+	int pos = 0;  // 当前数据索引
+	std::ofstream ofs(path);
+	for (auto data : lib)
+	{
+		std::replace(data.begin(), data.end(), '/', '\t');
+		ofs << data;
+		pos++;
+		if (pos < lib.size())
+			ofs << "\n";
+	}
+	ofs.close();
+}
