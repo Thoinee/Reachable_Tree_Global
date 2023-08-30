ad41e97809ebf3d5ffb5ab4630347978b936e1de
Thoin <814632021@qq.com>
2023-08-30 09:10:15 +0800

    update code

--- deduplication.h
+++ deduplication.h
@@ -1,6 +1,6 @@
 #pragma once
 /**************************************************
-* @This function is used to delete same data.
+* @Function  : be used to delete same data.
 * @Input     : several datas path
 * @Output    : one dataset with data deduplication
 * @Up-to-date: 2023-08-16 08:27
@@ -17,20 +17,22 @@ using std::vector;
 using std::string;
 using std::unordered_map;
 
-typedef unordered_map<string, string> mymap;
+typedef unordered_map<string, string> Mymap;
+typedef unsigned int Uint;
 
-static const vector<string> dataset = { "001", "010", "011", "100", "101", "110", "111", 
-										"112", "121", "122", "211", "212", "221", "222"};  // 指定待融合数据集文件
+//static const vector<string> dataset = { "001", "010", "011", "100", "101", "110", "111", 
+//										"112", "121", "122", "211", "212", "221", "222",
+//										"223", "232", "233", "322", "323", "332", "333"};  // 指定待融合数据集文件
 
-//static const vector<int> dataset = { 333 };  // 指定待融合数据集文件
+static const vector<string> dataset = { "122_"/*, "011"*/ };  // 指定待融合数据集文件
 
 /** 数据去重 */
 unordered_map<string, string>* Deduplication(const string folder_path) 
 {
-	mymap* data = new mymap;  // 存放数据
-	unsigned int diff = 0;  // 异同的数据量
-	unsigned int same = 0;  // 相同的数据量
-	unsigned int index = 0; // 当前数据索引
+	Mymap* data = new Mymap;  // 存放数据
+	Uint diff = 0;  // 异同的数据量
+	Uint same = 0;  // 相同的数据量
+	Uint index = 0; // 当前数据索引
 
 	for(auto it = dataset.begin(); it != dataset.end(); ++it){
 		string path = folder_path + "\\" + *it + "\\Dataset.txt";  // 任务文件夹路径
@@ -55,10 +57,13 @@ unordered_map<string, string>* Deduplication(const string folder_path)
 			// key值相同 保留h值小的元素
 			if (data->find(key) != data->end()) {
 				auto h = data->find(key)->second;  // 获取h值
+				/*if (std::stof(val) == std::stof(h)) {
+					same++;
+				}*/
 				if (std::stof(val) < std::stof(h)) {
 					data->at(key) = val;
 				}
-				++same;
+				same++;
 				continue;
 			}
 			// 异同
@@ -80,7 +85,7 @@ void ExportFinalData(unordered_map<string, string>* lib, const char* path)
 {
 	int pos = 0;  // 当前数据索引
 
-	std::ofstream ofs(path);
+	std::ofstream ofs(path); // 去重数据输出流
 
 	for (auto it : *lib){
 		ofs << it.first << '\t' << it.second;
